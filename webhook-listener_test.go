6c0e3d2eaba04a455a873f98ea7c3ef0183b5a7d
acef16pilot <acef16pilot@gmail.com>
2015-12-14 08:29:02 +0000

    Add Example JSON string for testing

--- webhook-listener_test.go
+++ webhook-listener_test.go
@@ -3,6 +3,8 @@ package main
 import (
     "testing"
 )
+//-- "{"onEntityEvent":{"eventSource":"urn:webhook:entity:Contact:update","callingSessionId":"U2015111600017554","eventTime":"2015-11-16 14:50:47Z","actionBy":"admin","actionByName":"System Administrator","entity":"Contact","record":{"h_pk_id":"103","h_jobtitle":"test","h_datelastmodified":"2015-11-16T14:50:47"}}}"
+var jsonTestString string = ""
 
 func TestHTTP(t *testing.T) {
 	t.Errorf("Error 1")
